9b2a3f7d17eeac66d3f7f1211469703bb4487d31
bilalanwar1 <88374606+bilalanwar1@users.noreply.github.com>
2024-02-21 22:38:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Question no 1
+// store a person in a variable
+var person_name = "Bilal";
+console.log("Hello ".concat(person_name, ", would you like to learn some Python today?"));
---
+++ main.ts
@@ -0,0 +1,5 @@
+//Question no 1
+// store a person in a variable
+
+let person_name:string = "Bilal";
+console.log(`Hello ${person_name}, would you like to learn some Python today?`);
