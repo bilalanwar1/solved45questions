051a3f43cc4035a03f9cf6c8669a7aaf88e8818b
bilalanwar1 <88374606+bilalanwar1@users.noreply.github.com>
2024-02-26 20:40:56 +0500

    Add files via upload

---
+++ ques4.js
@@ -0,0 +1,2 @@
+var famous_person = "Einstine";
+console.log(famous_person, "Once said, A person who never made amistake nerver tried anything new.");
---
+++ ques4.ts
@@ -0,0 +1,2 @@
+let famous_person:string="Einstine";
+console.log(famous_person, "Once said, A person who never made amistake nerver tried anything new.")
